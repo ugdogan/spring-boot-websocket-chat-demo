3f261b448dacc1fe777965cc2e210cf1b5e81686
Ugur <ug.dogan@outlook.de>
2022-09-20 10:27:12 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootwebsocketchatdemo/SpringBootWebsocketChatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebsocketchatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketChatDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebsocketChatDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebsocketchatdemo/SpringBootWebsocketChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebsocketchatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketChatDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
